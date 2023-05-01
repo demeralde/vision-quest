9b52a203e161c57d4f967c59cc557ec05e33d295
Daniel Emeralde <9282837+dspacejs@users.noreply.github.com>
2023-05-01 12:42:45 +0930

    Improve <Image> hover styles

--- src/components/Image/styles.ts
+++ src/components/Image/styles.ts
@@ -1,6 +1,8 @@
 import tw, { styled } from "twin.macro";
 import { GatsbyImage } from "gatsby-plugin-image";
 
-const Image = styled(GatsbyImage)(() => [tw`shrink-0 grow xl:rounded-lg shadow-md`]);
+const Image = styled(GatsbyImage)(() => [
+	tw`shrink-0 grow xl:rounded-lg shadow-md hover:shadow-lg transition`,
+]);
 
 export { Image };
